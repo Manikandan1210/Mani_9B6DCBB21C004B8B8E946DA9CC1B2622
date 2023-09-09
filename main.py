0180f07b98b071a95d07a507bfc0ec16f629167c
Manikandan1210 <144481621+Manikandan1210@users.noreply.github.com>
2023-09-09 17:32:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# 1.1 Implement a recursive to calculate the factorial of a given number
+def factorial(n):
+
+  # single line to find factorial
+  return 1 if (n == 1 or n == 0) else n * factorial(n - 1)
+
+
+# Driver Code
+num = 5
+print("Factorial of", num, "is", factorial(num))
